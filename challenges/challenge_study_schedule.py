55d045e68805575d192d9c3fd2aa7774849c16f3
Willian Prado <willian_prado@hotmail.com>
2022-03-24 14:47:06 -0300

    feat: add `challenge_study_schedule` function

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,16 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not isinstance(target_time, int):
+        return None
+
+    result = 0
+    for period in permanence_period:
+        starting_time = period[0]
+        ending_time = period[1]
+        
+        if not (isinstance(starting_time, int) and isinstance(ending_time, int)):
+            return None
+
+        if starting_time <= target_time <= ending_time:
+            result += 1
+
+    return result
