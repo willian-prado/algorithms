3170fdc1d8a704d2ceda129824cf68a2b420e87c
Willian Prado <willian_prado@hotmail.com>
2022-03-25 20:05:47 -0300

    feat: add `merge_sort` function

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,13 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        middle = len(arr) // 2
+        left = arr[:middle]
+        right = arr[middle:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        merge(left, right, arr)
+
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
