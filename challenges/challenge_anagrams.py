d5f25cf984cc4c1f5b17e7b96d669660d4039aa6
Willian Prado <willian_prado@hotmail.com>
2022-03-25 20:06:38 -0300

    feat: add `merge` function

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -9,5 +9,29 @@ def merge_sort(arr):
 
         merge(left, right, arr)
 
+
+def merge(left, right, merged):
+    left_index = 0
+    right_index = 0
+    merged_index = 0
+
+    while (left_index < len(left) and right_index < len(right)):
+        if left[left_index] < right[right_index]:
+            merged[merged_index] = left[left_index]
+            left_index += 1
+        else:
+            merged[merged_index] = right[right_index]
+            right_index += 1
+
+        merged_index += 1
+
+    for left_index in range(left_index, len(left)):
+        merged[merged_index] = left[left_index]
+        merged_index += 1
+
+    for right_index in range(right_index, len(right)):
+        merged[merged_index] = right[right_index]
+        merged_index += 1
+
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
